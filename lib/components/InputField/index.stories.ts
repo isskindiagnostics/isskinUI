fd8237860fcd2738b248ea833e5d009af778fda5
Larissa Soares <larissa.soares@neugelb.com>
2025-07-05 15:57:03 +0200

    feat: add input field component

---
+++ lib/components/InputField/index.stories.ts
@@ -0,0 +1,68 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+
+import { IconsArray } from "../../types/icons";
+import InputField from "./index";
+
+const meta: Meta<typeof InputField> = {
+  title: "Components/InputField",
+  component: InputField,
+  tags: ["autodocs"],
+  argTypes: {
+    label: {
+      control: "text",
+      description: "The label text displayed above the date input.",
+    },
+    error: {
+      control: "text",
+      description: "Error message shown when the input date is invalid.",
+    },
+    disabled: {
+      control: "boolean",
+      description: "Disables the date input when true.",
+    },
+    placeholder: {
+      control: "text",
+      description: "Placeholder text shown inside the date input field.",
+    },
+    hint: {
+      control: "text",
+      description:
+        "Optional hint text displayed below the input field to guide the user.",
+    },
+    icon: {
+      control: { type: "select" },
+      options: [...IconsArray, undefined],
+      defaultValue: "Eye",
+      description: "Select the icon to display alongside the content.",
+    },
+    width: {
+      control: "text",
+      description:
+        "Sets the input width. Accepts any valid CSS width value (e.g., '100%', '200px', 'fit-content').",
+    },
+    type: {
+      control: { type: "select" },
+      options: ["text", "email", "password", "number", "tel"],
+      defaultValue: "text",
+      description:
+        "Specifies the input type (e.g., text, email, password, etc.).",
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof InputField>;
+
+export const Default: Story = {
+  args: {
+    label: "Label",
+    icon: undefined,
+    disabled: false,
+    error: "Invalid date",
+    placeholder: "Placeholder",
+    hint: "Optional hint",
+    width: "380px",
+    type: "text",
+  },
+};
