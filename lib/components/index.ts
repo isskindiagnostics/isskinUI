beb36e4addcc009dbaadc861ab49539c15d50b08
Larissa Soares <larissa.soares@neugelb.com>
2025-07-06 17:47:11 +0200

    refactor: add component to index export and add input ref

--- lib/components/index.ts
+++ lib/components/index.ts
@@ -1,3 +1,19 @@
+import Button from "./Button";
+import Checkbox from "./Checkbox";
+import DatePicker from "./DatePicker";
+import IconLink from "./IconLink";
+import InputField from "./InputField";
 import Notification from "./Notification";
+import Radio from "./Radio";
+import Switch from "./Switch";
 
-export { Notification };
+export {
+  Button,
+  Checkbox,
+  DatePicker,
+  IconLink,
+  InputField,
+  Notification,
+  Radio,
+  Switch,
+};
