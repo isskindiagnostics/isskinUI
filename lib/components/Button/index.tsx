f3cb29f04588ff5b64182ea38a39764d5eae9a26
Larissa Soares <larissa.soares@neugelb.com>
2025-08-19 22:51:15 +0200

    fix: optional vairant for button

--- lib/components/Button/index.tsx
+++ lib/components/Button/index.tsx
@@ -4,11 +4,16 @@ import styles from "./index.module.css";
 
 export type ButtonProps = PropsWithChildren<
   ButtonHTMLAttributes<HTMLButtonElement> & {
-    variant: "solid" | "outlined";
+    variant?: "solid" | "outlined";
   }
 >;
 
-const Button = ({ variant, className, children, ...rest }: ButtonProps) => {
+const Button = ({
+  variant = "solid",
+  className,
+  children,
+  ...rest
+}: ButtonProps) => {
   return (
     <button
       className={`${styles.button} ${className} ${styles[variant]}`}
